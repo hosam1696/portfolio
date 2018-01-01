2d24ae58ca4083fb5e06c5728ef9a78ba1f05493
hosamsam <hosamelnabawy1996@gmail.com>
2018-01-02 01:31:01 +0200

    make the website more responsive

--- dist/main.js
+++ dist/main.js
@@ -8,6 +8,19 @@ if ('serviceWorker' in navigator) {
             console.warn('service worker is failed to register!', erdr)
         })
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const showSide = document.getElementById('l-side')
+    const leftSide = document.getElementById('side-info')
+    showSide.addEventListener('click', event => {
+        let checked = event.target.checked
+        if (checked)
+            leftSide.style.left = 0
+        else
+            leftSide.style.left = '-100%'    
+    })
+    console.log('dom loaded');
+})
 /* web worker
 if (window && window.Worker) {
     let webWorker = new Worker('./ww.js')
